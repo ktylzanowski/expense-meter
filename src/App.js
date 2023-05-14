6ba258b0752c96e6aa47781b7054458e37493fcf
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-05-14 23:43:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+function App() {
+  return (
+    <div>
+      <NewExpense></NewExpense>
+      <Expenses></Expenses>
+    </div>
+
+  );
+}
+
+export default App;
