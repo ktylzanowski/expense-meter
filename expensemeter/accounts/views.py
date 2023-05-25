60bf5e9d0b2206fe4c355d76c7cfdb1f763cd4e3
Kacper Tylzanowski <ktylzanowski@gmail.com>
2023-05-25 14:17:49 +0200

    register 1 part

--- expensemeter/accounts/views.py
+++ expensemeter/accounts/views.py
@@ -2,6 +2,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializer import UserSerializer
+from rest_framework import status
+from .models import MyUserManager, MyUser
 
 class getRoutes(APIView):
     def get(self, request):
@@ -23,4 +26,13 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-    
+
+
+class RegisterView(APIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        email = self.request.data['email']
+        password = self.request.data['password']
+        MyUser.objects.create_user(email=email, password=password)
+        return Response(status=status.HTTP_201_CREATED)
--- src/components/User/RegisterForm.js
+++ src/components/User/RegisterForm.js
@@ -1,6 +1,8 @@
 import "./LoginForm.css";
 import Card from "../UI/Card";
 import React, { useState } from "react";
+import axios from "axios";
+
 const RegisterForm = (props) => {
   const [login, setLogin] = useState("")
   const [password, setPassword] = useState("")
@@ -18,13 +20,26 @@ const RegisterForm = (props) => {
     setPassword2(event.target.value)
   }
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const userDate = {
+      email: login,
+      password: password,
+    };
+    axios.post("http://localhost:8000/api/register", userDate)
+    .catch((error) => {
+      console.error(error);
+    })
+    console.log(userDate)
+  }
+
   const handleCancel = () => {
     props.onCancel();
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <Card className="login-form">
           <div className="login-form__controls">
             <div className="login-form__control">
